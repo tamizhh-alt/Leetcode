e581564563ca538fc0f3ce7d1516c7c3db85bed7
tamizhh-alt <vtamizh335@gmail.com>
2025-03-24 12:33:30 +0530

    Time: 3 ms (99.8%), Space: 56.9 MB (52.71%) - LeetHub

--- 0811-number-of-subarrays-with-bounded-maximum/0811-number-of-subarrays-with-bounded-maximum.java
+++ 0811-number-of-subarrays-with-bounded-maximum/0811-number-of-subarrays-with-bounded-maximum.java
@@ -1,20 +1,17 @@
-class Solution 
-{
-    public int numSubarrayBoundedMax(int[] nums, int left, int right) 
-    {
+class Solution {
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
         int start =-1;
         int end =-1;
         int result =0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]>right){
+                end =i;
                 start =i;
-                end=i;
-                continue;
             }
             if(nums[i]>=left){
-                end=i;
+                end =i;
             }
-            result+= end-start;
+            result+=end-start;
         }
         return result;
     }
