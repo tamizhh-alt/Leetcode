35c9c6aeb60a74cb7edf7d16e81399cc3b34ea8c
tamizhh-alt <vtamizh335@gmail.com>
2025-04-17 10:29:19 +0530

    Time: 0 ms (100%), Space: 41.6 MB (21.27%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int l = 0;
+        int count = 0;
+        List<Integer> list = new ArrayList<>();
+
+        char[] ch = s.toCharArray();
+        for(int i=0; i<ch.length; i++){
+            if(ch[i] == 'L') l++;
+            else l--;
+            if(l==0) list.add(0);
+        }
+
+    return list.size();
+    }
+}
