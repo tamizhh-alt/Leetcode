fd4fa4abe0375a30e0681c59048c488964529d82
tamizhh-alt <vtamizh335@gmail.com>
2025-04-26 23:49:12 +0530

    Time: 2 ms (100%), Space: 43.6 MB (27.31%) - LeetHub

---
+++ 1547-destination-city/1547-destination-city.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> startingCities = new HashSet<>();
+        for (List<String> path : paths) {
+            startingCities.add(path.get(0));
+        }
+        for (List<String> path : paths) {
+            if (!startingCities.contains(path.get(1))) {
+                return path.get(1);
+            }
+        }
+        return ""; 
+    }
+}
